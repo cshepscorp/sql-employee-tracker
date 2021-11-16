f4834983fb4bc1d5a062924e2a1774c303ac1ee9
cshepscorp <sheppard.christy@gmail.com>
2021-11-16 16:55:34 -0500

    added figlet to fancy up the intro

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const db = require('./db/connection');
 const inquirer = require('inquirer');
 // npm install console.table --save
 const cTable = require('console.table');
+// npm install figlet for styling intro copy
+var figlet = require('figlet');
 
 function startSearch() {
   return inquirer.prompt([
@@ -365,5 +367,20 @@ function quit() {
   process.exit();
 }
 
-startSearch();
+figlet.text('Employee Tracker!', {
+  font: 'Slant',
+  horizontalLayout: 'default',
+  verticalLayout: 'default',
+  width: 100,
+  whitespaceBreak: true
+}, function(err, data) {
+  if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+  }
+  console.log(data);
+  console.log("\n");
+  startSearch();
+});
 
